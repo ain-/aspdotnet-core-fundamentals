db8b7ee813e0845bee8eb2defddf3731f163fa3b
Ain <ain.salula@gmail.com>
2017-01-11 06:48:52 +0200

    detail view, links, nameof examples

--- src/OdeToFood/Services/RestaurantData.cs
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -1,11 +1,14 @@
 ﻿using OdeToFood.Entities;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace OdeToFood.Services
 {
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -23,6 +26,11 @@ namespace OdeToFood.Services
         public IEnumerable<Restaurant> GetAll()
         {
             return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
         }
 
         List<Restaurant> _restaurants;
