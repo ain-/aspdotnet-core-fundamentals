9f848092471359bc64634162b1265c68638ceafd
Ain <ain.salula@gmail.com>
2017-01-11 07:16:46 +0200

    creating a restaurant

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -34,5 +34,10 @@ namespace OdeToFood.Controllers
             return View(model);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
--- src/OdeToFood/Entitities/Restaurant.cs
+++ src/OdeToFood/Entitities/Restaurant.cs
@@ -1,9 +1,19 @@
 ﻿namespace OdeToFood.Entities
 {
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        Japanese,
+        American
+    }
+
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
 
     }
 }
