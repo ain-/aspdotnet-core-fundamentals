0827443ba839d19765f12e4f567aef20376373e3
Ain <ain.salula@gmail.com>
2017-01-12 01:59:57 +0200

    view components

---
+++ src/OdeToFood/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreetingViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default", model);
+        }
+
+    }
+}
