339dc5f58aa021d8dd0bbc98cd8c56900f1f9b48
Ain <ain.salula@gmail.com>
2017-01-12 07:03:51 +0200

    creating a new user, fix versioning

---
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Entitities;
+using OdeToFood.ViewModels;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+
+                var createResult = await _userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
