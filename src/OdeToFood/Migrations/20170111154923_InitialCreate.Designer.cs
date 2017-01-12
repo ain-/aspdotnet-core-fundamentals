f2bec624e7c37417378a3024520186283c787fd5
Ain <ain.salula@gmail.com>
2017-01-12 05:50:47 +0200

    migrations

---
+++ src/OdeToFood/Migrations/20170111154923_InitialCreate.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OdeToFood.Entitities;
+using OdeToFood.Entities;
+
+namespace OdeToFood.Migrations
+{
+    [DbContext(typeof(OdeToFoodDbContext))]
+    [Migration("20170111154923_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("OdeToFood.Entities.Restaurant", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cuisine");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(80);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Restaurants");
+                });
+        }
+    }
+}
