d7c6e299fc5aea662b9ae071d8dea91b3efed0e7
Ain <ain.salula@gmail.com>
2017-01-12 08:51:34 +0200

    log in and out

--- src/OdeToFood/Controllers/AccountController.cs
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -47,5 +47,42 @@ namespace OdeToFood.Controllers
 
             return View();
         }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginResult = await _signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, model.RememberMe, false);
+                if (loginResult.Succeeded)
+                {
+                    if (Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Could not login");
+            return View(model);
+        }
+
     }
 }
